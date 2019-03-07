188033b201f277ac766b6144d47ada628551d14c
Mark Mazumder <mmaz@users.noreply.github.com>
2019-03-07 09:49:40 -0500

    ::snake::

--- csv_chrome.py
+++ csv_chrome.py
@@ -1,4 +1,5 @@
 import subprocess
+import datetime
 import csv
 
 get_template = """osascript -e 'set text item delimiters to linefeed' -e 'tell app "google chrome" to {} of tabs of window {} as text'"""
@@ -10,11 +11,12 @@ def getTitles(windowId : int) -> str:
 
 def getWindow(windowId : int) -> [(str,str)]:
     getproc = lambda s : subprocess.run(s, shell=True, check=True, stdout=subprocess.PIPE)
-    urlp   = getproc(getUrls(windowId))
-    titlep = getproc(getTitles(windowId))
-    urls   = urlp.stdout.decode("utf-8").splitlines()
-    titles = titlep.stdout.decode("utf-8").splitlines()
-    return list(zip(titles,urls))
+    urlp    = getproc(getUrls(windowId))
+    titlep  = getproc(getTitles(windowId))
+    urls    = urlp.stdout.decode("utf-8").splitlines()
+    titles  = titlep.stdout.decode("utf-8").splitlines()
+    titles_urls = zip(titles,urls)
+    return list(map(lambda tu: [windowId] + list(tu), titles_urls))
 
 def getAllWindows(maxWindow : int) -> [(str,str)]:
     """maxWindow is 1-indexed"""
@@ -25,6 +27,21 @@ def getAllWindows(maxWindow : int) -> [(str,str)]:
     return allWindows
 
 def windows2csv(fn, windows):
-    with open(fn, 'w',) as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+    with open(fn, 'w',) as fh:
+        writer = csv.writer(fh, quoting=csv.QUOTE_MINIMAL)
         writer.writerows(windows)
+
+def countWindows() -> int:
+    chromeWindows = """osascript -e 'tell app "google chrome" to count of windows as text'"""
+    countp = subprocess.run(chromeWindows, shell=True, check=True, stdout=subprocess.PIPE)
+    counts = countp.stdout.decode("utf-8").strip()
+    return int(counts)
+
+def savetabs(): 
+    num_windows = countWindows() #1-indexed
+    windows = getAllWindows(num_windows)
+    fn = "alltabs_{}.csv".format(datetime.datetime.now().date())
+    windows2csv(fn, windows)
+
+if __name__ == "__main__":
+    savetabs()
